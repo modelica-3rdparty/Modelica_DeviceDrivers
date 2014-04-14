c30a9d38fa3ad03580b1702fd33b2d8698c22277
Bernhard Thiele <bernhard.thiele@dlr.de>
2014-04-14 14:14:01 +0200

    Added MDDUtil as dependency to ITI_* libraries. Might fix issue reported in #16

---
+++ Resources/src/ITI_ModelicaDeviceDrivers/MDDSoftingCANWrapper.c
@@ -0,0 +1,11 @@
+/** Wrapping header-only library MDDSoftingCAN.h into a DLL.
+ *
+ * @file
+ * @author		Bernhard Thiele <bernhard.thiele@dlr.de>
+ * @since		2014-04-12
+ * @copyright Modelica License 2
+ * 
+ *
+*/
+#define MDDSOFTINGCANUSECMAKE
+#include "../../Include/MDDSoftingCAN.h"
