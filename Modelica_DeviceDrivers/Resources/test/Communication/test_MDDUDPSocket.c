ef42518a279de375f2d07a80e88562458e9939db
tbeu <tc@tbeu.de>
2015-03-23 22:23:32 +0100

    Add test for MDD_udpGetReceivedBytes

--- Modelica_DeviceDrivers/Resources/test/Communication/test_MDDUDPSocket.c
+++ Modelica_DeviceDrivers/Resources/test/Communication/test_MDDUDPSocket.c
@@ -1,10 +1,10 @@
 /** Test for MDDUDPSocket.
-*
-* @file
-* @author      Bernhard Thiele <bernhard.thiele@dlr.de>
-* @since       2012-05-30
-* @copyright Modelica License 2
-* @test Test for MDDUDPSocket.h.
+ *
+ * @file
+ * @author      Bernhard Thiele <bernhard.thiele@dlr.de>
+ * @since       2012-05-30
+ * @copyright Modelica License 2
+ * @test Test for MDDUDPSocket.h.
 */
 
 #include <stdio.h>
@@ -17,6 +17,7 @@ int main(void) {
     char sendMessage[80];
     const char *recMessage;
     int i;
+    int recBytes;
 
     recSocket = MDD_udpConstructor(10002, 80);
     if (recSocket == 0) {
@@ -26,17 +27,19 @@ int main(void) {
 
     sendSocket = MDD_udpConstructor(0,0);
     if (sendSocket == 0) {
+        MDD_udpDestructor(recSocket);
         perror("sendSocket == NULL\n");
         exit(1);
     }
 
     for (i=0; i < 10; i++) {
-        sprintf(sendMessage, "Current i is %i\n", i);
+        sprintf(sendMessage, "Current i is %i", i);
         //MDD_udpSend(sendSocket, "127.0.0.1", 10002, sendMessage, strlen(sendMessage));
         MDD_udpSend(sendSocket, "127.0.0.1", 10002, sendMessage, 80);
         //Sleep(1);
+        recBytes = MDD_udpGetReceivedBytes(recSocket);
         recMessage = MDD_udpRead(recSocket);
-        printf("Received: %s\n", recMessage);
+        printf("Received %d bytes: %s\n", recBytes, recMessage);
     }
 
     MDD_udpDestructor(recSocket);
