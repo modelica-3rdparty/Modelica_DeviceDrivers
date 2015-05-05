f4d92d5fc55f9db156fcc4522ec620d972b71432
tbeu <tbeu@users.noreply.github.com>
2015-05-05 13:30:55 +0200

    Run Astyle cosmetics

--- Modelica_DeviceDrivers/Resources/test/Util/ModelicaUtilities.c
+++ Modelica_DeviceDrivers/Resources/test/Util/ModelicaUtilities.c
@@ -8,45 +8,41 @@
 */
 
 void ModelicaMessage(const char *string) {
-	printf("%s", string);
+    printf("%s", string);
 }
 
-
-void ModelicaFormatMessage(const char *string,...) {
-   va_list p_arg;
-   va_start(p_arg,string);
-   vprintf(string,p_arg);
-   va_end(p_arg);
+void ModelicaFormatMessage(const char *string, ...) {
+    va_list p_arg;
+    va_start(p_arg,string);
+    vprintf(string,p_arg);
+    va_end(p_arg);
 }
-
-
 
 void ModelicaError(const char *string) {
-   fprintf (stderr, "%s", string);
-   exit(1);
+    fprintf (stderr, "%s", string);
+    exit(1);
 }
 
-
-void ModelicaFormatError(const char *string,...) {
-   va_list p_arg;
-   va_start(p_arg,string);
-   vfprintf(stderr, string,p_arg);
-   va_end(p_arg);
-   exit(1);
+void ModelicaFormatError(const char *string, ...) {
+    va_list p_arg;
+    va_start(p_arg,string);
+    vfprintf(stderr, string,p_arg);
+    va_end(p_arg);
+    exit(1);
 }
-
 
 char* ModelicaAllocateString(size_t len) {
-   char *res = ModelicaAllocateStringWithErrorReturn(len);
-   if(!res)
-      ModelicaFormatError("%s:%d: ModelicaAllocateString failed\n", __FILE__, __LINE__);
-  return res;
+    char *res = ModelicaAllocateStringWithErrorReturn(len);
+    if (!res) {
+        ModelicaFormatError("%s:%d: ModelicaAllocateString failed\n", __FILE__, __LINE__);
+    }
+    return res;
 }
 
 char* ModelicaAllocateStringWithErrorReturn(size_t len) {
-   char* res = (char*) malloc(len + 1);
-   if (res != NULL) {
-      res[len] = '\0';
-   }
-   return res;
+    char* res = (char*) malloc(len + 1);
+    if (res != NULL) {
+        res[len] = '\0';
+    }
+    return res;
 }
