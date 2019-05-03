e4d9e528ad1c385cfd531c5dc7feeaa711939a6b
Bernhard Thiele <bernhard.thiele@dlr.de>
2019-05-03 14:55:45 +0200

    More succinct dummy library creation (#285)

---
+++ Modelica_DeviceDrivers/Resources/src/DummyLibraries/MDDDummyLibrary.c
@@ -0,0 +1,10 @@
+/** Dummy library for uniform treatment of Modelica external function in windows and linux.
+ *
+ * @file
+ * @author		bernhard-thiele
+ * @since		2019-04-05
+ *
+ * See README.md for the rationale behind this approach.
+*/
+
+static void MDD_dummy(void);
